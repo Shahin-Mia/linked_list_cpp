0a42129d473bca0f79b2cf095100531167a2d6f9
Shahin-Mia <shahin.cmt2@gmail.com>
2025-08-24 16:09:22 +0600

    reverse list item done

--- reverse_linked_list.cpp
+++ reverse_linked_list.cpp
@@ -1,8 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void listItemInput(int val, Node *&head, Node *&tail)
+{
+    while (cin >> val)
+    {
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+}
+
+void reverse(Node *tmp)
+{
+    if (tmp == NULL)
+    {
+        return;
+    }
+    reverse(tmp->next);
+    cout << tmp->val << ' ';
+}
+
 int main()
 {
-    // code
-
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    listItemInput(val, head, tail);
+    reverse(head);
     return 0;
 }
