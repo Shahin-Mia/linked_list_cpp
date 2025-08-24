68962b75bf5220ad4b6388ec5a3490226f69dc93
Shahin-Mia <shahin.cmt2@gmail.com>
2025-08-24 14:51:09 +0600

    problem of 7.5 solved

---
+++ problem5.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+int countListItem(Node *head)
+{
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+void listItemInput(int val, Node *&head, Node *&tail)
+{
+    while (cin >> val)
+    {
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+}
+
+int main()
+{
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
+    int val;
+    listItemInput(val, head1, tail1);
+    listItemInput(val, head2, tail2);
+
+    if (countListItem(head1) == countListItem(head2))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
